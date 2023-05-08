cb0bb224a1ab5460961fba9b219eabf432ea140e
Anton Nilsson <anton.smeliy@gmail.com>
2023-05-08 22:15:58 +0200

    Base functions

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Word!"
+
+@app.route("/meetings")
+def meetings():
+    return {"pic": 1, "Location": "Östermalm", "Time": "13:13", "Description": "Getting drunk with the bois"}
