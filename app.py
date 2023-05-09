709f7a598a31a7b800676dde39beef448740ef45
Anton Nilsson <anton.smeliy@gmail.com>
2023-05-09 21:06:55 +0200

    Working database

--- app.py
+++ app.py
@@ -2,24 +2,22 @@ from flask import Flask, request, jsonify
 import sqlite3
 app = Flask(__name__)
 
-cur = sqlite3.connect("meetings.db").cursor()
-
 # Scehma CREATE TABLE meeting(name TINYTEXT, description MEDIUMTEXT, picture int, Location TINYTEXT, time TINYTEXT);
 # One row of data in the db {'Beer with the boies', 'We are gonna get drunk and have fun', '1', 'Andralong', '2023-05-12 18:00'}
 
 @app.route("/")
 def home():
-    return
+    return "Test"
 
 @app.route("/meeting", methods = ["Get", "Post"])
 def meeting():
-    if request.method == "Get":
+    if request.method == "GET": 
+        cur = sqlite3.connect("meetings.db").cursor()
         try:
             data = cur.execute("SELECT * FROM meeting")
-            
-            return jsonify(data)
+            return jsonify(data.fetchall())
         except:
             return "Server side error", 501
         
-    if request.method == "Post":
+    if request.method == "POST":
         return
